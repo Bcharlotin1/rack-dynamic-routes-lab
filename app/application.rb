c19c1bf207db91f7b22c5e0fbc9d83e3c4e7ba5e
bcharlotin1 <biancacharlotin1@gmail.com>
2021-03-01 18:27:20 -0500

    commit tree cdd3ca7438e7195cdef0dab79d7e1867d084ab49

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Application
+@@items = []
+    def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+      
+
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last #turn /items/Sofigrry into fig
+
+            if item = @@items.find {|obj| obj.name == item_name}
+        
+                resp.write item.price.to_s
+            else
+                resp.write "Item not found"
+                resp.status = 400
+
+        end
+
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+  
+      resp.finish
+    end
+  end 
